7725d79b0c8611199d46b532fed8d58aaa79bd8e
Vlad Hromadskyi <s32732@pjwstk.edu.pl>
2026-03-24 17:58:17 +0100

    feat: add base abstract Equipment and User classes

---
+++ cw3APBD/Models/Equipment.cs
@@ -0,0 +1,17 @@
+﻿namespace cw3APBD.Models;
+
+public abstract class Equipment
+{
+    public Guid Id { get; } = Guid.NewGuid();
+    public string Name { get; set; }
+    public bool IsAvailable { get; set; } = true;
+    public string SerialNumber { get; set; }
+
+    protected Equipment(string name, string serialNumber)
+    {
+        Name = name;
+        SerialNumber = serialNumber;
+    }
+
+    public abstract string GetSpecifications();
+}
---
+++ cw3APBD/Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace cw3APBD.Models;
+
+public abstract class User
+{
+    public Guid Id { get; } = Guid.NewGuid();
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public abstract int MaxRenatalLimit { get; }
+
+    protected User(string fName, string lName)
+    {
+        FirstName = fName;
+        LastName = lName;
+    }
+}
