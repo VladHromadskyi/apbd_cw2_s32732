64a6c15d517546d1c8a3655522aab7a9425ea040
Vlad Hromadskyi <s32732@pjwstk.edu.pl>
2026-03-24 19:19:55 +0100

    feat: implement IPenaltyCalculator and DailyPenaltyCalculator

---
+++ cw3APBD/Services/DailyPenaltyCalculator.cs
@@ -0,0 +1,16 @@
+﻿namespace cw3APBD.Services;
+
+public class DailyPenaltyCalculator : IPenaltyCalculator
+{
+    private const decimal DailyRate = 10.5m;
+    public decimal Calculate(DateTime DueDate, DateTime ReturnDate)
+    {
+        if (ReturnDate <= DueDate)
+        {
+            return 0;
+        }
+
+        int overdueDays = (ReturnDate - DueDate).Days;
+        return overdueDays * DailyRate;
+    }
+}
---
+++ cw3APBD/Services/IPenaltyCalculator.cs
@@ -0,0 +1,8 @@
+﻿using System.Runtime.InteropServices.JavaScript;
+
+namespace cw3APBD.Services;
+
+public interface IPenaltyCalculator
+{
+    decimal Calculate(DateTime DueDate, DateTime ReturnDate);
+}
