187b13e2bdae9ac7edf9fce96b9184337d4b6478
Vlad Hromadskyi <s32732@pjwstk.edu.pl>
2026-03-24 21:00:23 +0100

    feat: implement RentalManager with business rules validation

---
+++ cw3APBD/Services/RentalManager.cs
@@ -0,0 +1,63 @@
+﻿using cw3APBD.Models;
+
+namespace cw3APBD.Services;
+
+public class RentalManager
+{
+    private readonly List<Equipment> _equipmentList = new();
+    private readonly List<User> _users = new();
+    private readonly List<Rental> _rentals = new();
+    private readonly IPenaltyCalculator _penaltyCalculator;
+
+    public RentalManager(IPenaltyCalculator penaltyCalculator)
+    {
+        _penaltyCalculator = penaltyCalculator;
+    }
+
+    public void AddEquipment(Equipment item)
+    {
+        _equipmentList.Add(item);
+    }
+
+    public void AddUser(User user)
+    {
+        _users.Add(user);
+    }
+
+    public void RentItem(Equipment equipment, User user, int days)
+    {
+        if (!equipment.IsAvailable)
+        {
+            throw new InvalidOperationException($"Equipment '{equipment.Name}' already busy.");
+        }
+
+        int activeRentalsCount = _rentals.Count(r => r.UserId == user.Id && r.ReturnDate == null);
+        if (activeRentalsCount >= user.MaxRentalLimit)
+        {
+            throw new InvalidOperationException($"Limit exceeded! {user.FirstName} can take maks {user.MaxRentalLimit} items.");
+        }
+
+        equipment.IsAvailable = false;
+        Rental rental = new Rental(user.Id, equipment.Id, days);
+        Console.WriteLine($"Success: {equipment.Name} was given to {user.LastName}. Return due to: {rental.DueDate:dd.MM.yyyy}");
+    }
+
+    public void ReturnItem(Guid itemId)
+    {
+        var rental = _rentals.FirstOrDefault(r => r.EquipmentId == itemId && r.ReturnDate == null);
+
+        if (rental == null)
+        {
+            throw new Exception("Record of active rental is not found");
+        }
+        var item = _equipmentList.First(e => e.Id == itemId);
+        
+        rental.ReturnDate = DateTime.Now;
+        item.IsAvailable = true;
+
+        // Считаем штраф через наш сервис
+        decimal penalty = _penaltyCalculator.Calculate(rental.DueDate, rental.ReturnDate.Value);
+        
+        Console.WriteLine($"Item {item.Name} returned. Penalty: {penalty:0.00} PLN");
+    }
+}
